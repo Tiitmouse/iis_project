dd62c831664cdd1578034411a250f89fb7d8023f
Tiitmouse <lkovacic380@gmail.com>
2025-03-03 16:17:21 +0100

    added upload endpoint

--- Server/main.go
+++ Server/main.go
@@ -1,1 +1,17 @@
 package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.POST("/upload", handleXMLUpload)
+	r.Run(":8088")
+}
+
+func handleXMLUpload(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{"message": "bokic"})
+}
