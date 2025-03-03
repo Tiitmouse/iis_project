e7b6c33d5d0e5ac0bd89fff92183479634c67753
Tiitmouse <lkovacic380@gmail.com>
2025-03-03 17:04:14 +0100

    uploaded file

--- Server/main.go
+++ Server/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"path/filepath"
 
 	"github.com/gin-gonic/gin"
 )
@@ -13,5 +14,18 @@ func main() {
 }
 
 func handleXMLUpload(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{"message": "bokic"})
+
+	file, err := c.FormFile("file")
+	if err != nil {
+		c.String(http.StatusBadRequest, "get file err: %s", err.Error())
+		return
+	}
+
+	filename := filepath.Base(file.Filename)
+	if err := c.SaveUploadedFile(file, filename); err != nil {
+		c.String(http.StatusBadRequest, "upload file err: %s", err.Error())
+		return
+	}
+
+	c.String(http.StatusOK, "File %s uploaded successfully", file.Filename)
 }
