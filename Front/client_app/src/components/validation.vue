efd84c31515b0d86cd54a44365d8860f87e924c0
Tiitmouse <lkovacic380@gmail.com>
2025-03-09 16:57:03 +0100

    validation choice and upload file

--- Front/client_app/src/components/validation.vue
+++ Front/client_app/src/components/validation.vue
@@ -1,5 +1,33 @@
-<template>hi from validator</template>
+<!-- eslint-disable vue/max-attributes-per-line -->
+<!-- eslint-disable vue/html-self-closing -->
+<!-- eslint-disable vue/html-indent -->
+<template>
+  <div>
+    <H3>Validate with:</H3>
+        <v-radio-group v-model="validationType" inline>
+          <v-radio label="XSD" value="xsd"></v-radio>
+          <v-radio label="RelaxNG" value="rng"></v-radio>
+        </v-radio-group>
+  </div>
+  <div>
+    <v-file-upload
+      clearable
+      density="compact"
+      title="Drag and drop .xml file"
+      variant="compact"
+    ></v-file-upload>
+  </div>
+</template>
 
 <script lang="ts" setup>
-//
+import { ref } from 'vue'
+import { VFileUpload } from "vuetify/labs/VFileUpload"
+
+const validationType = ref("xsd")
 </script>
+
+<style>
+.v-sheet {
+    background: #5373B3;
+}
+</style>
