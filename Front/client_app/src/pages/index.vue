a294942043eff8fffb3877880edcc4511adb8b90
Tiitmouse <lkovacic380@gmail.com>
2025-03-19 11:17:08 +0100

    tabs switched to app bar

--- Front/client_app/src/pages/index.vue
+++ Front/client_app/src/pages/index.vue
@@ -1,38 +1,51 @@
 <!-- eslint-disable vue/max-attributes-per-line -->
 <!-- eslint-disable vue/html-self-closing -->
+
 <template>
-  <v-card class="topCard">
-    <v-card-title class="text-center justify-center py-6">
-      <h1 class="font-weight-bold text-h2 text-basil">
-        Interoperability of information systems
-      </h1>
-    </v-card-title>
+  <v-app>
+    <v-app-bar app color="indigo" dark>
+      <v-toolbar-title>My App</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn v-for="item in items" :key="item" text="true" @click="setTab(item)">
+        {{ item }}
+      </v-btn>
+    </v-app-bar>
 
-    <v-tabs v-model="tab" bg-color="transparent" grow>
-      <v-tab v-for="item in items" :key="item" :text="item" :value="item"></v-tab>
-    </v-tabs>
+    <v-navigation-drawer v-model="drawer" app temporary>
+      <v-list>
+        <v-list-item-group v-for="item in items" :key="item">
+          <v-list-item @click="setTab(item)">
+            <v-list-item-content>
+              <v-list-item-title>{{ item }}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
 
-    <v-tabs-window v-model="tab">
-      <v-tabs-window-item v-for="item in items" :key="item" :value="item">
-        <v-card flat class="whitebg">
+    <v-main>
+      <v-container>
+        <v-card class="whitebg">
           <v-card-text class="cardText">
-            <component :is="currentComponent"></component>
+            <component :is="currentComponent" v-if="tab !== ''"></component>
           </v-card-text>
         </v-card>
-      </v-tabs-window-item>
-    </v-tabs-window>
-  </v-card>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
 <script setup lang="ts">
-import { ref, computed } from 'vue'
-import Validation from '../components/validation.vue'
-import ApiConJwt from '../components/apiConJwt.vue'
-import Weather from '../components/weather.vue'
-import Soap from '../components/soap.vue'
-import JAXB from '../components/jaxb.vue'
+import { ref, computed, defineAsyncComponent } from 'vue'
 
-const tab = ref('Menus')
+const Validation = defineAsyncComponent(() => import('../components/validation.vue'))
+const ApiConJwt = defineAsyncComponent(() => import('../components/apiConJwt.vue'))
+const Weather = defineAsyncComponent(() => import('../components/weather.vue'))
+const Soap = defineAsyncComponent(() => import('../components/soap.vue'))
+const JAXB = defineAsyncComponent(() => import('../components/jaxb.vue'))
+
+const tab = ref('') 
+const drawer = ref(false) 
 
 const items = [
   'Validation',
@@ -55,12 +68,16 @@ const currentComponent = computed(() => {
     case 'API con JWT':
       return ApiConJwt
     default:
-      return { template: '<div> default value </div>', props: ['text'] }
+      return { template: '<div> Select a tab </div>' }
   }
 })
 
-
+const setTab = (selectedTab: string) => {
+  tab.value = selectedTab
+  drawer.value = false 
+}
 </script>
+
 
 <style>
 .topCard {
